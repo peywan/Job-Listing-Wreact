929a4335328e27cf2ffbacbb4a67d536a0288ad3
Peywan Sabir <peywan1999@icloud.com>
2024-04-10 10:49:35 +0200

    Job list, uploading progress on practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+    const name = 'Peywan'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const peywanInfo =
+        {
+            name: name,
+            age: 25,
+            hobbies: ['football', 'basketball', 'cricket']
+        }
+
+    return (
+
+        <div className="flex ">
+
+            <h1 className="text-3xl font-bold underline rounded-xl border-8 grey border-black bg-gray-300 px-20 m-20">
+                Hello {name}
+                <p>Namn: {peywanInfo.name}</p>
+                <p>Ålder: {peywanInfo.age}</p>
+            </h1>
+        </div>
+    )
 }
 
-export default App
