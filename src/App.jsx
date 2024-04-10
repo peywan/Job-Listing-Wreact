67a09d80d156601ceb677280ddf1cd3b3dd24f56
Peywan Sabir <peywan1999@icloud.com>
2024-04-10 12:06:10 +0200

    boiler plate for my job listing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-export default function App() {
-
-const names = ["John", "Doe", "Smith", "Jones", "Williams", "Brown", "Davis",]
-const loggedIn = true
-const styles = {
-    color: 'purple',
-    fontSize: '202px',
-}
+const App = () => {
     return (
-
-        <div className="flex ">
-            <div>
-                <p style={{color: 'red', fontSize: 90, fontWeight: 'bold'}}>hello styled john</p>
-                <ul>
-                    {names.map((name, namesIndex) => (<li
-                        key={namesIndex}>
-                        {name}</li>))}
-                </ul>
-
-                { loggedIn ? <h1 style={styles}>hello member</h1> : <h1>hello guest</h1>}
-            </div>
-        </div>)
+        <div>App</div>
+    )
 }
-
+export default App
