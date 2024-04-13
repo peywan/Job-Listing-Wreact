817c0b2fa719dbdaf3b7ad800b2668755bf1d729
Peywan Sabir <peywan1999@icloud.com>
2024-04-13 23:20:04 +0200

    added proptypes and changed code structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,24 @@
-const Hero = ({ title = "Welcome to React Jobs", subtitle = "find your next job" }) => {
-    return (
-        <section className="bg-indigo-700 py-20 mb-4">
-            <div
-                className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-            >
-                <h1
-                    className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-                >
-                    {title}
+import PropTypes from 'prop-types'; // Import PropTypes
 
-                </h1>
-                <div className="text-center">
-                    <p className="my-4 text-xl text-white">
-                        {subtitle}
-                    </p>
+const Hero = (props) => {
+    return (
+        <section className='bg-indigo-700 py-20 mb-4'>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
+                <div className='text-center'>
+                    <h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>
+                        {props.title}
+                    </h1>
+                    <p className='my-4 text-xl text-white'>{props.subtitle}</p>
                 </div>
             </div>
         </section>
-    )
-}
-export default Hero
+    );
+};
+
+// Add prop type validation
+Hero.propTypes = {
+    title: PropTypes.string.isRequired, // Ensure title is a required string
+    subtitle: PropTypes.string.isRequired, // Ensure subtitle is a required string
+};
+
+export default Hero;
