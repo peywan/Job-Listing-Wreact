a54fa0e3fa47a2ac5f9b634b876f22ebcd5c714e
Peywan Sabir <peywan1999@icloud.com>
2024-04-17 14:13:03 +0200

    Code clean: Added card to homecards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
-const Card = ({children}) => {
+import PropTypes from 'prop-types';
+
+const Card = ({ children }) => {
     return (
         <div className="bg-gray-100 shadow-md rounded-lg p-6 mb-4">{children}</div>
     );
 };
 
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
 export default Card;
- 
