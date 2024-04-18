41fe7837ba79db1a9796f9f36a70ee109f88daf3
Peywan Sabir <peywan1999@icloud.com>
2024-04-18 15:09:55 +0200

    Feat: Added see more or less on job article

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -19,13 +19,18 @@ const JobListing = ({job}) => {
                 <div className="mb-5">
                     {description}
                 </div>
-                <button className="text-indigo-500 mb-5 hover:text-indigo-600">
+                <button onClick={() => (setShowFullDescription((prevState) =>
+                        !prevState)
+                    )}
+                    className="text-indigo-500 mb-5 hover:text-indigo-600">
                     {showFullDescription ? 'Less' : 'More'}
                 </button>
 
                 <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
 
-                <div className="border border-gray-100 mb-5"></div>
+
+
+                <div className="border border-gray-100- mb-5"></div>
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
