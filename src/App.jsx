c95736a76828ac1531ae1446dc67319a9be800a6
Peywan Sabir <peywan1999@icloud.com>
2024-04-18 09:33:08 +0200

    app.jsx only component with new file ViewAllJobs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Navbar.jsx'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from "./components/JobListings.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
 
 const App = () => {
     return (
@@ -10,17 +11,7 @@ const App = () => {
             <Hero title="React Jobs" subtitle="Find your next developer job." />
             <HomeCards />
             <JobListings />
-
-
-
-            <section className="m-auto max-w-lg my-10 px-6">
-                <a
-                    href="jobs.html"
-                    className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-                >View All Jobs</a
-                >
-            </section>
-
+            <ViewAllJobs />
         </>
     )
 }
